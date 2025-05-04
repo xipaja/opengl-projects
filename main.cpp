27092c9a8db5de589d9ceecb297dbd05135d41a3
xipaja <xipaja@gmail.com>
2025-05-04 15:46:03 -0700

    First commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <SDL3/SDL.h>
+
+void InitializeProgram() {
+
+}
+
+void MainLoop() {
+
+}
+
+void CleanUp() {
+
+}
+
+int main() {
+
+    InitializeProgram();
+
+    MainLoop();
+
+    CleanUp();
+    
+    return 0;
+}
