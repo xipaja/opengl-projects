82d48e14ac96013172d85b25160a9063dad36d56
xipaja <xipaja@gmail.com>
2025-05-20 21:25:46 -0700

    More cubes

--- website/main.cpp
+++ website/main.cpp
@@ -123,6 +123,19 @@ int main() {
         -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
 
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f), 
+        glm::vec3( 2.0f,  5.0f, -15.0f), 
+        glm::vec3(-1.5f, -2.2f, -2.5f),  
+        glm::vec3(-3.8f, -2.0f, -12.3f),  
+        glm::vec3( 2.4f, -0.4f, -3.5f),  
+        glm::vec3(-1.7f,  3.0f, -7.5f),  
+        glm::vec3( 1.3f, -2.0f, -2.5f),  
+        glm::vec3( 1.5f,  2.0f, -2.5f), 
+        glm::vec3( 1.5f,  0.2f, -1.5f), 
+        glm::vec3(-1.3f,  1.0f, -1.5f)  
+    };
+
     // Store vertex data, managed by VBO
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
@@ -182,10 +195,10 @@ int main() {
         customShader.use();
 
         // Model matrix - rotate on x-axis
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::rotate(model, (SDL_GetTicks() / 1000.0f) * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-        int modelLocation = glGetUniformLocation(customShader.id, "model");
-        glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
+        // glm::mat4 model = glm::mat4(1.0f);
+        // model = glm::rotate(model, (SDL_GetTicks() / 1000.0f) * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+        // int modelLocation = glGetUniformLocation(customShader.id, "model");
+        // glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
 
         // View matrix - moving camera backwards = moving scene forward in -z direction
         glm::mat4 view = glm::mat4(1.0f);
@@ -200,7 +213,15 @@ int main() {
         glUniformMatrix4fv(projLocation, 1, GL_FALSE, glm::value_ptr(projection));
 
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        for (unsigned int i = 0; i < 10; i++) {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, (SDL_GetTicks() / 1000.0f) * glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            int modelLocation = glGetUniformLocation(customShader.id, "model");
+            glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
         SDL_GL_SwapWindow(g_GraphicsWindow);
         pollEvents();
