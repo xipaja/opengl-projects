a7be15e3c0cadc73f20035574475f72c25ff13df
xipaja <xipaja@gmail.com>
2025-06-29 10:35:14 -0700

    Add CMake

---
+++ build/CMakeFiles/output.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Ximena/Desktop/opengl_projects/src/glad.c" "CMakeFiles/output.dir/src/glad.c.obj" "gcc" "CMakeFiles/output.dir/src/glad.c.obj.d"
+  "C:/Users/Ximena/Desktop/opengl_projects/src/camera.cpp" "CMakeFiles/output.dir/src/camera.cpp.obj" "gcc" "CMakeFiles/output.dir/src/camera.cpp.obj.d"
+  "C:/Users/Ximena/Desktop/opengl_projects/src/cube.cpp" "CMakeFiles/output.dir/src/cube.cpp.obj" "gcc" "CMakeFiles/output.dir/src/cube.cpp.obj.d"
+  "C:/Users/Ximena/Desktop/opengl_projects/src/geometry.cpp" "CMakeFiles/output.dir/src/geometry.cpp.obj" "gcc" "CMakeFiles/output.dir/src/geometry.cpp.obj.d"
+  "C:/Users/Ximena/Desktop/opengl_projects/src/main.cpp" "CMakeFiles/output.dir/src/main.cpp.obj" "gcc" "CMakeFiles/output.dir/src/main.cpp.obj.d"
+  "C:/Users/Ximena/Desktop/opengl_projects/src/mesh.cpp" "CMakeFiles/output.dir/src/mesh.cpp.obj" "gcc" "CMakeFiles/output.dir/src/mesh.cpp.obj.d"
+  "C:/Users/Ximena/Desktop/opengl_projects/src/model.cpp" "CMakeFiles/output.dir/src/model.cpp.obj" "gcc" "CMakeFiles/output.dir/src/model.cpp.obj.d"
+  "C:/Users/Ximena/Desktop/opengl_projects/src/rectangle.cpp" "CMakeFiles/output.dir/src/rectangle.cpp.obj" "gcc" "CMakeFiles/output.dir/src/rectangle.cpp.obj.d"
+  "C:/Users/Ximena/Desktop/opengl_projects/src/shader.cpp" "CMakeFiles/output.dir/src/shader.cpp.obj" "gcc" "CMakeFiles/output.dir/src/shader.cpp.obj.d"
+  "C:/Users/Ximena/Desktop/opengl_projects/src/vertices.cpp" "CMakeFiles/output.dir/src/vertices.cpp.obj" "gcc" "CMakeFiles/output.dir/src/vertices.cpp.obj.d"
+  "C:/Users/Ximena/Desktop/opengl_projects/src/window.cpp" "CMakeFiles/output.dir/src/window.cpp.obj" "gcc" "CMakeFiles/output.dir/src/window.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/output.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/output.dir/src/camera.cpp.obj"
+  "CMakeFiles/output.dir/src/camera.cpp.obj.d"
+  "CMakeFiles/output.dir/src/cube.cpp.obj"
+  "CMakeFiles/output.dir/src/cube.cpp.obj.d"
+  "CMakeFiles/output.dir/src/geometry.cpp.obj"
+  "CMakeFiles/output.dir/src/geometry.cpp.obj.d"
+  "CMakeFiles/output.dir/src/glad.c.obj"
+  "CMakeFiles/output.dir/src/glad.c.obj.d"
+  "CMakeFiles/output.dir/src/main.cpp.obj"
+  "CMakeFiles/output.dir/src/main.cpp.obj.d"
+  "CMakeFiles/output.dir/src/mesh.cpp.obj"
+  "CMakeFiles/output.dir/src/mesh.cpp.obj.d"
+  "CMakeFiles/output.dir/src/model.cpp.obj"
+  "CMakeFiles/output.dir/src/model.cpp.obj.d"
+  "CMakeFiles/output.dir/src/rectangle.cpp.obj"
+  "CMakeFiles/output.dir/src/rectangle.cpp.obj.d"
+  "CMakeFiles/output.dir/src/shader.cpp.obj"
+  "CMakeFiles/output.dir/src/shader.cpp.obj.d"
+  "CMakeFiles/output.dir/src/vertices.cpp.obj"
+  "CMakeFiles/output.dir/src/vertices.cpp.obj.d"
+  "CMakeFiles/output.dir/src/window.cpp.obj"
+  "CMakeFiles/output.dir/src/window.cpp.obj.d"
+  "liboutput.dll.a"
+  "output.exe"
+  "output.exe.manifest"
+  "output.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/output.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
